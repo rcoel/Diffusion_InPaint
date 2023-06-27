92c60091025d4471b6a13a690fe87a9e6e6d65eb
Reevan Coelho <reevancoelho@gmail.com>
2023-06-27 22:01:58 +0530

    Initial commit

---
+++ download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+(
+mkdir -p data/pretrained
+cd data/pretrained
+
+gdown https://drive.google.com/uc?export=download&id=1-N-f2bOzsczQ6KLCeaTgs329dq0eXERy
+gdown https://drive.google.com/uc?export=download&id=1zLf3Ut4P2qfj1duLKzviLv5Uig68qqPv
+
+gdown https://drive.google.com/uc?export=download&id=1-1Q6C39V-Uskm0aylTA3uTS1QwXpH5V0
+gdown https://drive.google.com/uc?export=download&id=1-Dtx5OiYlG4g5yna9gt6PfoqdsdWSUKU
+)
+
+# data
+(
+gdown https://drive.google.com/uc?export=download&id=1-FjmPLPqq5bPdRVjzijqPrMWB3Vs-ukU
+unzip data.zip
+rm data.zip
+)
