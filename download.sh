5b97bc8eaf21f3093eac19f120cf501dba3dc81b
Reevan Coelho <reevancoelho@gmail.com>
2023-06-30 23:44:05 +0530

    changes in bash file

--- download.sh
+++ download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 (
-mkdir -p data/pretrained
-cd data/pretrained
+mkdir -p models
+cd models
 
 wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt # Trained by OpenAI
 wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt # Trained by OpenAI
